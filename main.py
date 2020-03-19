be427abe838faf3a3554c9d79f438412bde452aa
tudu-rahul <rahultudu923@gmail.com>
2020-03-20 01:42:19 +0530

    corrected response

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from    flask   import request
 import  logging
 import  requests
 import  ssl
+import json
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -23,14 +24,6 @@ app = Flask(__name__)
 logging.info("python code started")
 
 
-def arrange(response):
-    with app.app_context():
-        res = make_response(response.json())
-    for key, value in response.headers.items():
-        res.headers[key] = value
-    return res
-
-
 @app.route("/v1")
 def working():
     return {"response": "api gateway service running"}
@@ -38,47 +31,56 @@ def working():
 
 @app.route("/v1/otp", methods=["GET"])
 def otp():
-    return arrange(requests.get("http://127.0.0.1:7002/v1/otp", params=request.args))
+    response = requests.get("http://127.0.0.1:7002/v1/otp", params=request.args)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/otp/verify", methods=["GET"])
 def otp_verify():
-    return arrange(requests.get("http://127.0.0.1:7002/v1/otp/verify", params=request.args))
+    response = requests.get("http://127.0.0.1:7002/v1/otp/verify", params=request.args)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/createuser", methods=["POST"])
 def createuser():
-    return arrange(requests.post("http://127.0.0.1:7002/v1/createuser", data=request.form))
+    response = requests.post("http://127.0.0.1:7002/v1/createuser", data=request.form)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/login", methods=["GET"])
 def login():
-    return arrange(requests.get("http://127.0.0.1:7002/v1/login", params=request.args))
+    response = requests.get("http://127.0.0.1:7002/v1/login", params=request.args)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/book", methods=["POST"])
 def book():
-    return arrange(requests.post("http://127.0.0.1:7004/v1/book", data=request.form, headers=request.headers))
+    response = requests.post("http://127.0.0.1:7004/v1/book", data=request.form, headers=request.headers)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/cancel", methods=["POST"])
 def cancel():
-    return arrange(requests.post("http://127.0.0.1:7004/v1/cancel", data=request.form, headers=request.headers))
+    response = requests.post("http://127.0.0.1:7004/v1/cancel", data=request.form, headers=request.headers)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/fare", methods=["GET"])
 def fare():
-    return arrange(requests.get("http://127.0.0.1:7004/v1/fare", params=request.args, headers=request.headers))
+    response = requests.get("http://127.0.0.1:7004/v1/fare", params=request.args, headers=request.headers)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/endTrip", methods=["POST"])
 def endTrip():
-    return arrange(requests.post("http://127.0.0.1:7004/v1/endTrip", data=request.form, headers=request.headers))
+    response = requests.post("http://127.0.0.1:7004/v1/endTrip", data=request.form, headers=request.headers)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 @app.route("/v1/getDistance", methods=["GET"])
 def getDistance():
-    return arrange(requests.get("http://127.0.0.1:7004/v1/getDistance", params=request.args, headers=request.headers))
+    response = requests.get("http://127.0.0.1:7004/v1/getDistance", params=request.args, headers=request.headers)
+    return Response(response=response.content, headers=dict(response.headers))
 
 
 if __name__ == '__main__':
